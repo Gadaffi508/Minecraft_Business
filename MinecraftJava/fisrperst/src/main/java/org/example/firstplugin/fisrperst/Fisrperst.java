f2d9cd720b31166be002bb4a89a4aa19c0daa402
Yusuf <yusuf_zeki1234@hotmail.com>
2024-04-04 23:32:12 +0300

    player joined to message added

--- MinecraftJava/fisrperst/src/main/java/org/example/firstplugin/fisrperst/Fisrperst.java
+++ MinecraftJava/fisrperst/src/main/java/org/example/firstplugin/fisrperst/Fisrperst.java
@@ -2,19 +2,29 @@ package org.example.firstplugin.fisrperst;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Fisrperst extends JavaPlugin {
+public final class Fisrperst extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        //Server console write
         Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&',"Start Plugin"));
-
+        getServer().getPluginManager().registerEvents(this,this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event)
+    {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&',"A player has joined the server"));
     }
 }
